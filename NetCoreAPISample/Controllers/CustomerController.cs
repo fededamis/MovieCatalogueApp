6a0846c1b9d5270398f6ce13acdbab23e4726ef4
Federico <fede_damis@hotmail.com>
2024-02-26 09:54:25 -0300

    Added test endpoint

---
+++ NetCoreAPISample/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreAPISample.Models;
+
+namespace NetCoreAPISample.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ILogger<CustomerController> _logger;
+
+        public CustomerController(ILogger<CustomerController> logger)
+        {
+            _logger = logger;
+        }        
+
+        [HttpGet]
+        [Route("test")]
+        public string GetCustomer()
+        {
+            return "test";
+        }
+
+    }
+}
