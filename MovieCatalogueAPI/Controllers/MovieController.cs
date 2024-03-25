9b32c33f09c800c50e25c4839a9c53d92adfd085
Federico <fede_damis@hotmail.com>
2024-03-25 13:53:25 -0300

    Add MovieCatalogue API

---
+++ MovieCatalogueAPI/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieCatalogueAPI.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly ILogger<MovieController> _logger;
+
+        public MovieController(ILogger<MovieController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("GetMovies")]
+        public string GetMovies()
+        {
+            return "test22";
+        }
+    }
+}
