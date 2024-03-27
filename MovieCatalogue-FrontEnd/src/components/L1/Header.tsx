52dba0a8b8662d2b0d432ecfdd9c0aedddd07ee8
Federico <fede_damis@hotmail.com>
2024-03-27 13:33:50 -0300

    Add fontawesome icon

--- MovieCatalogue-FrontEnd/src/components/L1/Header.tsx
+++ MovieCatalogue-FrontEnd/src/components/L1/Header.tsx
@@ -1,30 +1,37 @@
 import { CSSProperties, FC } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilm } from "@fortawesome/free-solid-svg-icons";
 
 export const headerHeight = "60px";
 
 const HeaderStyle: CSSProperties = {
-  height: headerHeight,
-  backgroundColor: "#20232a",
-  color: "#fff",
-  display: "flex",
-  flexDirection: "row",
-  alignItems: "center",
-  justifyContent: "center"
+    height: headerHeight,
+    backgroundColor: "#20232a",
+    color: "#fff",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
 };
 
 const HeaderIconStyle: CSSProperties = {
-  fontSize: "2rem",
-  paddingLeft: "1rem",
-  color: "cornflowerblue"
+    fontSize: "2rem",
+    paddingLeft: "1rem",
+    color: "cornflowerblue",
 };
 
 const Header: FC = () => {
-  return (
-    <div className="header" style={HeaderStyle}>         
-      <h1>Movie Catalogue </h1>   
-      <i className="bi bi-film" style={HeaderIconStyle}></i>      
-    </div>
-  );
+    return (
+        <div className="header" style={HeaderStyle}>
+            <h1>Movie Catalogue </h1>
+            <FontAwesomeIcon
+                icon={faFilm}
+                bounce
+                size="2xl"
+                style={HeaderIconStyle}
+            />
+        </div>
+    );
 };
 
 export default Header;
--- MovieCatalogue-FrontEnd/src/main.tsx
+++ MovieCatalogue-FrontEnd/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
