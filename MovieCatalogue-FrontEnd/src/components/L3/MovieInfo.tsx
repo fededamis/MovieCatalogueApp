66aadc3b401b2bae6d1931dea86d4f3efd81cb98
Federico <fede_damis@hotmail.com>
2024-04-03 16:39:13 -0300

    Fix movie info comp styles

--- MovieCatalogue-FrontEnd/src/components/L3/MovieInfo.tsx
+++ MovieCatalogue-FrontEnd/src/components/L3/MovieInfo.tsx
@@ -1,19 +1,23 @@
-import { FC } from "react";
+import { FC, CSSProperties } from "react";
 
 export type MovieInfoProps = {
-  label: string;
-  value: string;
+    label: string;
+    value: string;
+};
+
+const movieInfoStyle: CSSProperties = {
+  fontSize: "calc(0.7em + 0.5vw)",
 };
 
 const MovieInfo: FC<MovieInfoProps> = ({ label, value }) => {
-  return (
-    <div>
-      <span>
-        <strong>{`${label}: `}</strong>
-      </span>
-      <span>{value}</span>
-    </div>
-  );
+    return (
+        <div style={movieInfoStyle}>
+            <span>
+                <strong>{`${label}: `}</strong>
+            </span>
+            <span>{value}</span>
+        </div>
+    );
 };
 
 export default MovieInfo;
